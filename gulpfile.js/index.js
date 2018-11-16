6f50786a79a3bb03ac816ca0309a42583ae1114d
cieclarke <chris@cieclarke.com>
2018-11-16 15:50:00 +0000

    eslint included and it's basic configuration added

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,44 +1,42 @@
-var ejs = require('gulp-ejs');
-var rename = require('gulp-rename');
-var clean = require('gulp-clean');
-var gulp = require('gulp');
-var config = require('./../site-config/global.json')
-var flickr = require('./../lib/flickr')
+const ejs = require('gulp-ejs');
+const rename = require('gulp-rename');
+const clean = require('gulp-clean');
+const gulp = require('gulp');
+const globalConfig = require('./../site-config/global.json');
+const flickr = require('./../lib/flickr');
 
 gulp.task('photos.htm', () => {
-
     return Promise.all([flickr('61777036f4ecf11adb192f7156c6e92e').done((photos) => {
         return gulp.src('./templates/photos.ejs')
             .pipe(ejs({
-                config: config,
+                config: globalConfig,
                 images: photos
             }))
             .pipe(rename('photos.htm'))
-            .pipe(gulp.dest("./dist"))
-        })
+            .pipe(gulp.dest('./dist'));
+    })
     ]);
-
-})
+});
 
 gulp.task('clean', () => {
-    return gulp.src('dist', {read: false, allowEmpty: true})
+    return gulp.src('dist', { read: false, allowEmpty: true })
         .pipe(clean());
-})
+});
 
 gulp.task('index.htm', () => {
     return gulp.src('./templates/index.ejs')
         .pipe(ejs({
-            config: config
+            config: globalConfig
         }))
         .pipe(rename('index.htm'))
-        .pipe(gulp.dest("./dist"))
+        .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('deploy', () => {
     return gulp.src('dist/*')
-      .pipe(gulp.dest('/'));
-})
-  
+        .pipe(gulp.dest('/'));
+});
+
 gulp.task('default', gulp.series('clean', gulp.parallel('index.htm', 'photos.htm')));
 
 gulp.task('deploy', gulp.series('default'));
---
+++ test/test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+describe('Data', () => {
+    it('should have a length > 0', () => {
+        assert.equal([12, 12, 12].length, 3);
+    });
+});
