1e90dbcdf8d7ee1a4128d3977ee987efde8e85e7
cieclarke <chris@cieclarke.com>
2018-12-20 19:52:30 +0000

    ftp task update

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -3,13 +3,26 @@ const rename = require('gulp-rename');
 const clean = require('gulp-clean');
 const chmod = require('gulp-chmod');
 const gulp = require('gulp');
+const ftp = require('vinyl-ftp');
 const sass = require('gulp-sass');
 const globalConfig = require('./../site-config/global.json');
 const flickr = require('./../lib/flickr');
 const tumblr = require('./../lib/tumblr');
+const options = require('./../options.json');
+
+gulp.task('clean', () => {
+    return gulp.src('dist', { read: false, allowEmpty: true })
+        .pipe(clean());
+});
+
+gulp.task('css', () => {
+    return gulp.src('css/main.scss')
+        .pipe(sass())
+        .pipe(gulp.dest(options.build));
+});
 
 gulp.task('photos.htm', () => {
-    return Promise.all([flickr('61777036f4ecf11adb192f7156c6e92e').done((photos) => {
+    return Promise.all([flickr(options.flickr.api_key).done((photos) => {
         return gulp.src('./templates/photos.ejs')
             .pipe(ejs({
                 config: globalConfig,
@@ -20,17 +33,6 @@ gulp.task('photos.htm', () => {
             .pipe(gulp.dest('./dist'));
     })
     ]);
-});
-
-gulp.task('css', () => {
-    return gulp.src('css/main.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('clean', () => {
-    return gulp.src('dist', { read: false, allowEmpty: true })
-        .pipe(clean());
 });
 
 gulp.task('index.htm', () => {
@@ -44,7 +46,7 @@ gulp.task('index.htm', () => {
 });
 
 gulp.task('blog.htm', () => {
-    return Promise.all([tumblr('QZZoo1PTjzR6zJpJQibwFshmEYjkdBw780HlKNr3lQWIWwxbUU').done((blogs) => {
+    return Promise.all([tumblr(options.tumblr.api_key).done((blogs) => {
         return gulp.src('./templates/blog.ejs')
             .pipe(ejs({
                 config: globalConfig,
@@ -67,11 +69,17 @@ gulp.task('work.htm', () => {
         .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('deploy', () => {
-    return gulp.src('dist/*')
-        .pipe(gulp.dest('/'));
-});
-
 gulp.task('default', gulp.series('clean', gulp.parallel('index.htm', 'photos.htm', 'blog.htm', 'work.htm', 'css')));
 
-gulp.task('deploy', gulp.series('default'));
+gulp.task('deploy', () => {
+    const src = options.build.endsWith('/*') ? options.build : `${options.build}/*`;
+
+    const conn = ftp.create({
+        host: options.ftp.server.host,
+        user: options.ftp.server.user,
+        password: options.ftp.server.password
+    });
+
+    return gulp.src(src)
+        .pipe(conn.dest(options.ftp.server.remote));
+});
